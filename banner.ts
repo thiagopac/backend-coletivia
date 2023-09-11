00672cc27b67feb7270c6caf4d55108bea9f10f1
Thiago Pires <thiagopac@gmail.com>
2023-09-11 15:24:06 -0300

    Endpoint de listagem de análises de um mesmo documento

--- banner.ts
+++ banner.ts
@@ -7,7 +7,7 @@ export const banner =
   '      d8"     `Y8            IP\'`Yb           I8                                      \n' +
   "     d8'   8b  d8            I8  8I        88888888 gg                 gg              \n" +
   '    ,8I    "Y88P\'            I8  8\'           I8    ""                 ""            \n' +
-  "    I8'            ,ggggg,   I8 dP   ,ggg,    I8    gg      ggg    gg  gg     ,gggg,gg \n" +
+  "    I8'            ,ggggg,   I8 dP   ,ggg,    I8    gg      ggg    gg  gg     .gggg,gg \n" +
   '    d8            dP"  "Y8gggI8dP   i8" "8i   I8    88     d8"Yb   88bg88    dP"  "Y8I \n' +
   "    Y8,          i8'    ,8I  I8P    I8, ,8I  ,I8,   88    dP  I8   8I  88   i8'    ,8I \n" +
   "    `Yba,,_____,,d8,   ,d8' ,d8b,_  `YbadP' ,d88b,_,88,_,dP   I8, ,8I_,88,_,d8,   ,d8b,\n" +
